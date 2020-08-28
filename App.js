71dda90970915f91c5ebd56729d00ae93d0f82f2
anishrajan25 <anishrajan2708@gmail.com>
2020-08-28 19:42:46 +0530

    gender selector

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Slider, StatusBar } from 'react-native';
-import { Icon } from "react-native-elements";
+import {Slider, StyleSheet, Text, View, StatusBar, TouchableOpacity } from 'react-native';
+import {  Icon } from "react-native-elements";
 
 const App = () => {
 
   const [height, setHeight] = useState(150);
+  const [gender, setGender] = useState('');
 
   return(
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#080C1E" />
-      <View style={{flexDirection: 'row', height: 50, paddingTop: 5, paddingHorizontal: 20}}>
+      <View style={{flexDirection: 'row', paddingTop: 5, paddingHorizontal: 20, flex: 1}}>
         <View style={{ flex: 9}}>
           <Text style={{color: 'white', fontSize: 20}}>BMI Calculator</Text>
           <Text style={{color: 'white', fontSize: 12}}>by anishrajan</Text>
@@ -23,21 +24,49 @@ const App = () => {
           />
         </View>
       </View>
-      <View style={{justifyContent: 'center', flex: 1}}>
-        <View style={styles.inputView}>
-          <Text style={styles.inputHeading}>Height</Text>
-          <Text style={styles.text}>{String(height) + ' cm'}</Text>
-          <Slider
-            thumbTintColor='#25A1CF'
-            minimumTrackTintColor="#25A1CF"
-            style={{marginVertical: 10}}
-            step={1}
-            maximumValue={300}
-            onValueChange={(value) => setHeight(value)}
-            value={height}
+      <View style={{justifyContent: 'center', flex: 4, flexDirection: 'row'}}>
+        <TouchableOpacity 
+          style={{...styles.inputView, justifyContent: 'center', flex: 1, alignItems: 'center', backgroundColor: gender === 'male' ? 'rgba(24,29,52,1)': 'rgba(24,29,52,0.5)'}}
+          onPress={() => setGender('male')}
+        >
+          <Icon
+            size={50}
+            name='mars'
+            type='font-awesome-5'
+            color='#ffffff'  
           />
-        </View>
+          <Text style={{color: 'white', marginTop: 10}}>MALE</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={{...styles.inputView, justifyContent: 'center', flex: 1, alignItems: 'center', backgroundColor: gender === 'female' ? 'rgba(24,29,52,1)': 'rgba(24,29,52,0.5)' }}
+          onPress={() => setGender('female')}
+        >
+          <Icon
+            size={50}
+            name='venus'
+            type='font-awesome-5'
+            color='#ffffff'  
+          />
+          <Text style={{color: 'white', marginTop: 10}}>FEMALE</Text>
+        </TouchableOpacity>
       </View>
+      <View style={styles.inputView}>
+        <Text style={styles.inputHeading}>HEIGHT</Text>
+        <Text style={styles.text}>{String(height) + ' cm'}</Text>
+        <Slider
+          thumbTintColor='#25A1CF'
+          minimumTrackTintColor="#25A1CF"
+          style={{alignSelf: 'stretch'}}
+          thumbStyle={{ height: 20, width: 20}}
+          step={1}
+          minimumValue={1}
+          maximumValue={300}
+          onValueChange={(value) => setHeight(value)}
+          value={height}
+        />
+      </View>
+      <View style={{flex: 4 }}></View>
+      <View style={{flex: 1 }}></View>
     </View>
   );
 }
@@ -58,11 +87,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(24,29,52,0.5)',
     margin: 10,
     padding: 10,
-    borderRadius: 15
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    flex: 4
   },
   inputHeading: {
     color: "#ffffff",
-    textAlign: 'center',
     marginTop: 5,
     fontSize: 20
   }
